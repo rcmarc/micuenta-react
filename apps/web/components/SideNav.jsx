aa7814ced7e69e9ace2f64adf6e4069dd1e8b868
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-19 16:47:10 +0000

    added initial side nav code

--- apps/web/components/SideNav.jsx
+++ apps/web/components/SideNav.jsx
@@ -1,5 +1,79 @@
-function SideNav() {
-  return <p>SideNav</p>;
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import {
+  MdAccountCircle,
+  MdHomeFilled,
+  MdInfo,
+  MdSecurity,
+} from 'react-icons/md';
+
+const LINKS = [
+  {
+    text: 'Inicio',
+    href: '/',
+    icon: MdHomeFilled,
+  },
+  {
+    text: 'Perfil',
+    href: '/profile',
+    icon: MdAccountCircle,
+  },
+  {
+    text: 'Seguridad',
+    href: '/security',
+    icon: MdSecurity,
+  },
+  {
+    text: 'Acerca de',
+    href: '/about',
+    icon: MdInfo,
+  },
+];
+
+const setSelected = (link, pathName) => {
+  return Object.assign({}, link, { selected: link.href === pathName });
+};
+
+const SideNavLi = ({ key, selected, children }) => (
+  <li
+    key={key}
+    className={`${
+      selected ? 'bg-primary-100' : 'hover:bg-slate-200'
+    } rounded-r-full `}
+  >
+    {children}
+  </li>
+);
+
+const SideNavLink = ({ link }) => (
+  <Link href={link.href}>
+    <div className="flex py-3 px-8">
+      {React.createElement(link.icon, {
+        className: `text-2xl ${
+          link.selected ? 'text-primary-700' : 'text-foreground-light'
+        } mr-3`,
+      })}
+      <p className="font-medium tracking-wide text-slate-600">{link.text}</p>
+    </div>
+  </Link>
+);
+
+function SideNav({ className }) {
+  const router = useRouter();
+  return (
+    <nav className={className}>
+      <ul>
+        {LINKS.map((link) => setSelected(link, router.pathname)).map(
+          (link, index) => (
+            <SideNavLi key={index} selected={link.selected}>
+              <SideNavLink link={link} />
+            </SideNavLi>
+          )
+        )}
+      </ul>
+    </nav>
+  );
 }
 
 export default SideNav;
