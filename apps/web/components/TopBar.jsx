45780f1a52f506f6e00059931e1b7d394f4893e9
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-19 14:45:15 +0000

    added initial top bar

--- apps/web/components/TopBar.jsx
+++ apps/web/components/TopBar.jsx
@@ -1,5 +1,22 @@
+import Logo from './Logo';
+import LogoText from './LogoText';
+
 function TopBar() {
-  return <p>TopBar</p>;
+  return (
+    <div className="flex bg-slate-200 px-10">
+      <div className="py-2">
+        <Logo size={50} />
+      </div>
+      <div className="flex py-4">
+        <div className="pt-1">
+          <p className="text-foreground-light px-2 text-lg italic">MiCuenta</p>
+        </div>
+        <div>
+          <LogoText size={'2xl'} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TopBar;
