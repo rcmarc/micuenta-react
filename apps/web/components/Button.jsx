4205486341782a970bda33e58b8ca0a6605567e5
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-21 21:21:10 +0000

    added className to Button component

--- apps/web/components/Button.jsx
+++ apps/web/components/Button.jsx
@@ -2,7 +2,7 @@ function Button({ children, className, ...props }) {
   return (
     <button
       {...props}
-      className={`self-end rounded-lg border-2 border-transparent bg-sky-400 py-2 px-3 shadow-sm outline-none transition-[border-color_background-color_color] hover:border-blue-600 hover:bg-sky-500 hover:shadow-xl active:bg-sky-400 active:shadow-sm  ${className}`}
+      className={`rounded-lg border-2 border-transparent bg-sky-400 shadow-sm outline-none transition-[border-color_background-color_color] hover:border-blue-600 hover:bg-sky-500 hover:shadow-xl active:bg-sky-400 active:shadow-sm  ${className}`}
     >
       {children}
     </button>
