f3a663b01b24da7b9cf61643f06b06132de8a26f
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-21 19:47:25 +0000

    changed IndeterminateProgressBar color class name

--- apps/web/components/IndeterminateProgressBar.jsx
+++ apps/web/components/IndeterminateProgressBar.jsx
@@ -1,6 +1,6 @@
 function IndeterminateProgressBar() {
   return (
-    <div className="bg-primary-900 animate-progress-indeterminate absolute top-0 left-0 h-[5px] w-full origin-top-left" />
+    <div className="animate-progress-indeterminate absolute top-0 left-0 h-[5px] w-full origin-top-left bg-sky-700" />
   );
 }
 
