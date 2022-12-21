04fb72ab7d998788b745cc03fc87113e52f9699b
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-21 19:16:34 +0000

    removed abstract class names from FormOnlyLayout component

--- apps/web/layouts/FormOnlyLayout.jsx
+++ apps/web/layouts/FormOnlyLayout.jsx
@@ -5,7 +5,7 @@ import LogoText from '../components/LogoText';
 
 const FormContainer = ({ children }) => {
   return (
-    <div className="shadow-stale-700 bg-background xs:h-full xs:w-9/12 xs:rounded-lg relative h-screen w-full overflow-hidden py-10 px-[5%] shadow-2xl sm:w-7/12 sm:px-14 md:w-5/12 md:px-10 lg:w-4/12 xl:w-3/12">
+    <div className="shadow-stale-700 xs:h-full xs:w-9/12 xs:rounded-lg relative h-screen w-full overflow-hidden bg-slate-100 py-10 px-[5%] shadow-2xl sm:w-7/12 sm:px-14 md:w-5/12 md:px-10 lg:w-4/12 xl:w-3/12">
       {children}
     </div>
   );
@@ -21,9 +21,7 @@ function FormOnlyLayout({ children, helperText }) {
               <Logo />
             </HCenter>
             <HCenter>
-              <p className="text-foreground-light pr-2 pt-0.5 text-xl">
-                MiCuenta
-              </p>
+              <p className="pr-2 pt-0.5 text-xl text-slate-500">MiCuenta</p>
               <LogoText />
             </HCenter>
             <HCenter className="mt-3">
