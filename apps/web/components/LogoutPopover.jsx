944f4e09e8bf519b9adaf7862eaef3c9b03dfcaf
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-21 21:29:37 +0000

    implemented LogoutPopover component

---
+++ apps/web/components/LogoutPopover.jsx
@@ -0,0 +1,35 @@
+import { signOut, useSession } from 'next-auth/react';
+
+import Avatar from './Avatar';
+import Button from './Button';
+
+function LogoutPopover() {
+  const session = useSession();
+  const user = session.data?.user;
+
+  const onClick = () => {
+    signOut();
+  };
+
+  return (
+    <>
+      <div className="mt-12 flex flex-col items-center rounded-lg border-[1px] border-slate-200 p-4 shadow-md">
+        {user ? (
+          <>
+            <Avatar />
+            <p className="mt-1 w-max text-sm font-medium text-slate-400">
+              {user.email}
+            </p>
+            <Button onClick={onClick} className={'mt-4 px-2 py-1'}>
+              Cerrar Sessión
+            </Button>
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default LogoutPopover;
