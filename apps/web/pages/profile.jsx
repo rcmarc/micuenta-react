3064f9f889510d1e1c15da623f73fb06b0f0463c
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-23 20:51:44 +0000

    return user to context

--- apps/web/pages/profile.jsx
+++ apps/web/pages/profile.jsx
@@ -1,4 +1,8 @@
+import { ldap, toUser } from '@ucfgos/ldap';
+import { unstable_getServerSession } from 'next-auth';
+
 import MainLayout from '../layouts/MainLayout';
+import { authOptions } from './api/auth/[...nextauth]';
 
 function Profile() {
   return <p>Profile</p>;
@@ -7,3 +11,15 @@ function Profile() {
 Profile.getLayout = (page) => <MainLayout>{page}</MainLayout>;
 
 export default Profile;
+
+export const getServerSideProps = async (context) => {
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  );
+  const user = toUser(await ldap.fetchEntry(`mail=${session.user.email}`));
+  return {
+    props: { user },
+  };
+};
