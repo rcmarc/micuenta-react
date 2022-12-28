11d752b65b9b6859925d951ed47e74bfb09c0a40
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-28 21:34:44 +0000

    updated MainLayout

--- apps/web/layouts/MainLayout.jsx
+++ apps/web/layouts/MainLayout.jsx
@@ -8,7 +8,9 @@ function MainLayout({ children }) {
       <ProgressBar />
       <TopBar />
       <div className="flex flex-col md:mt-5 md:flex-row">
-        <SideNav className={'md:w-4/12 md:flex-col lg:w-3/12 xl:w-2/12'} />
+        <SideNav
+          className={'md:w-4/12 md:flex-col lg:mr-20 lg:w-3/12 xl:w-2/12'}
+        />
         {children}
       </div>
     </>
