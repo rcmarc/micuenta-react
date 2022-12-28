4f304b6ec20ffb974c3ccaa96d07ddb39c34382b
Marcos Carrera <ramoscarrer@gmail.com>
2022-12-28 21:39:33 +0000

    removed previous security component

--- apps/web/pages/security.jsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from '../layouts/MainLayout';
-
-function Security() {
-  return <p>Security</p>;
-}
-
-Security.getLayout = (page) => <MainLayout>{page}</MainLayout>;
-
-export default Security;
